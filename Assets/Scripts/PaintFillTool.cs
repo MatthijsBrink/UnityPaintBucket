8149afe15233ebf9f6fdc77dd738626529e953c9
Matthijs Brink <matthijs.brink@home.nl>
2019-02-23 17:57:04 +0100

    Created paint fill bucket

---
+++ Assets/Scripts/PaintFillTool.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class   PaintFillTool : MonoBehaviour, IPointerClickHandler {
+
+    public static Color ColorToSet;
+
+    Material Material;
+    Texture2D Image;
+    Texture2D ImageCopy;
+
+    // Use this for initialization
+    void Start () {
+        Image = GetComponent<Renderer>().sharedMaterial.mainTexture as Texture2D;
+        ImageCopy = new Texture2D(Image.width, Image.height, Image.format, Image.mipmapCount > 1);
+        ImageCopy.LoadRawTextureData(Image.GetRawTextureData());
+
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnApplicationQuit()
+    {
+        Image.LoadRawTextureData(ImageCopy.GetRawTextureData());
+        Image.Apply();
+    }
+
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+
+
+        GameObject pointerPosition = new GameObject();
+        pointerPosition.transform.position = hit.point;
+        pointerPosition.transform.SetParent(hit.collider.transform);
+
+        int newx = (int)(Image.width * (pointerPosition.transform.localPosition.x + 0.5f));
+        int newY = (int)(Image.height * (pointerPosition.transform.localPosition.y + 0.5f));
+        Destroy(pointerPosition);
+
+        ImageUtilss.FloodFillArea(Image, newx, newY, ColorToSet);
+        Image.Apply();
+    }    
+}
---
+++ Assets/Scripts/SetColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SetColor : MonoBehaviour, IPointerClickHandler {
+
+    public Color color;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PaintFillTool.ColorToSet = color;
+    }
+
+    // Use this for initialization
+    void Start () {
+        this.GetComponent<Renderer>().material.color = color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
